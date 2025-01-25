5fef1227759334fdcb37f7df72bc8f4600dc899d
AryanBisht1234 <driveescape2580@gmail.com>
2025-01-25 19:58:32 +0530

    adding media queries

--- src/Components/MainImage.jsx
+++ src/Components/MainImage.jsx
@@ -5,10 +5,10 @@ import  style from"./MainImage.module.css"
 import DownloadButton from '../Button/DownloadButton'
 function MainImage() {
   return (
-    <div style={{display:"flex",alignItems:"center",justifyContent:"space-between",paddingLeft:"1rem",paddingRight:"1rem"}}>
+    <div  style={{marginTop:"1rem",display:"flex",alignItems:"center",justifyContent:"space-between",paddingLeft:"1rem",paddingRight:"1rem"}}>
       <div style={{display:"flex",alignItems:"center"}}>
         <img src={y} style={{height:"2rem"}} className={style.container}/>
-        <img src={x} className={style.container}/>
+        <img src={x} className={style.container} />
       </div>
       <div style={{display:"flex",alignItems:"center"}}>
         <DownloadButton/>
